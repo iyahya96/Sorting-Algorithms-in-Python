3c52751d4e73a8cfccfd58443d65f8858721b405
iyahya96 <xfantomzx@gmail.com>
2025-09-11 16:29:48 +0500

    Update Count_sort.py
    write comments

--- Count_sort.py
+++ Count_sort.py
@@ -14,9 +14,13 @@
     return original_array'''
 
 
+'''Counting sort is an efficient, non-comparison based sorting algorithm particularly well-suited for sorting a collection of objects according to keys that are small, non-negative integers.
+It operates by determining the position of each element in the output array by counting the occurrences of each unique key value.'''
 def count_sort(given_array:list[int])->list[int]:
 
+    #determine max
     maximum_number = max(given_array)
+    #initialize counting array
     count_array = [0] * (maximum_number + 1)
 
     #Store Frequency
@@ -28,10 +32,13 @@ def count_sort(given_array:list[int])->list[int]:
         count_array[i] += count_array[i-1]
 
     count_array = [0] + count_array[:-1]
+    #initialize output array
     output = [0] * len(given_array)
 
+    #build output array
     for i in given_array:
         output[count_array[i]] = i
         count_array[i] += 1
     return output
+
 print(count_sort([1,0,3,1,3,1]))
